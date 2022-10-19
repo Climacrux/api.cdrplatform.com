59a10263f04daba65adacdf5a30694461b440490
Ewan Jones <egcjones@gmail.com>
2022-10-19 15:53:06 +0200

    fixing up some urls for django debug toolbar

--- cdrplatform/urls.py
+++ cdrplatform/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
+app_patterns = [
+    path("", include("cdrplatform.core.urls")),
 ]
+
+other_patterns = [
+    path("accounts/", include(("django.contrib.auth.urls", "auth"))),
+]
+if settings.ENABLE_DJANGO_ADMIN:
+    other_patterns.append(
+        path(settings.DJANGO_ADMIN_PATH, admin.site.urls),
+    )
+
+if settings.DEBUG:
+    other_patterns.append(
+        path("__debug__/", include("debug_toolbar.urls")),
+    )
+
+
+urlpatterns = app_patterns + other_patterns
