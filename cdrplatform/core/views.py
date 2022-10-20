809be8d63f28d2f9f70b725eb7261d3d03242f30
Ewan Jones <egcjones@gmail.com>
2022-10-20 14:37:13 +0200

    switching out typed django responses to the generic rest_framework Response which can handle multiple formats

--- cdrplatform/core/views.py
+++ cdrplatform/core/views.py
@@ -1,6 +1,6 @@
-from django.http import JsonResponse
 from rest_framework import serializers, status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 @api_view(("POST",))
@@ -33,5 +33,5 @@ def cdr_pricing(request):
     if request.method == "POST":
         serializer = InputSerializer(data=request.data)
         if serializer.is_valid():
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
