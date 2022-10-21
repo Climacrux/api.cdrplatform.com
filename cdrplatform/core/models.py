1b70839952133cd7845f1eeb21d1e08cf782b265
Fabienne Muff <fabienne.muff@sunrise.net>
2022-10-21 20:47:44 +0200

    added model classes

--- cdrplatform/core/models.py
+++ cdrplatform/core/models.py
@@ -1,3 +1,61 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class Invoice(models.Model):
+    user = models.ForeignKey()
+    invoice_id = models.CharField(max_length=10)
+    issued_date = models.DateField()
+    paid_date = models.DateField()
+    fees = models.PositiveIntegerField()
+    currency = models.CharField(max_length=3)
+
+
+class Certificate(models.Model):
+    removal_request = models.ForeignKey()
+    certificate_id = models.CharField(max_length=10)
+    issued_date = models.DateField()
+    name = models.CharField(max_length=50)
+
+
+class RemovalRequest(models.Model):
+    requested_date = models.DateField()
+    weight_unit = models.CharField(max_length=2)
+    invoice = models.ForeignKey()
+    currency = models.CharField(max_length=3)
+    user = models.ForeignKey()
+
+
+class ApiKey:
+    key = models.CharField(max_length=30)
+    user = models.ForeignKey()
+
+
+class RemovalRequestItem(models.Model):
+    removal_method = models.ForeignKey()
+    removal_request = models.ForeignKey()
+    cost = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField()
+
+
+class PartnerCertificate(models.Model):
+    partner = models.ForeignKey()
+    removal_request = models.ForeignKey()
+    certificate_id = models.CharField(max_length=30)
+    issued_date = models.DateField()
+    name = models.CharField(max_length=30)
+    removal_request_item = models.ForeignKey()
+
+
+class Partner(models.Model):
+    removal_method = models.ForeignKey()
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    website = models.URLField()
+    cost_per_tonne = models.PositiveIntegerField()
+    currency = models.CharField(max_length=3)
+
+
+class RemovalMethod(models.Model):
+    name = models.CharField(max_length=30)
