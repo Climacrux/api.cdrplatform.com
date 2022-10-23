7398fec8e52930b6e18c4d7c324a286c0208c393
Ewan Jones <egcjones@gmail.com>
2022-10-23 20:20:15 +0200

    converting pricing view to a class based view to make better use of inheritance and the django spectacular api generation

--- cdrplatform/core/urls.py
+++ cdrplatform/core/urls.py
@@ -1,17 +1,14 @@
 from django.urls import include, path
 
-from .views import cdr_pricing, cdr_removal_request
+from .views import CDRPricing, cdr_removal_request
 
 app_name = "core"
 
 cdr_routes = [
-    path("price/", cdr_pricing, name="cdr_price"),
+    path("price/", CDRPricing.as_view(), name="cdr_price"),
     path("", cdr_removal_request, name="cdr_request"),
 ]
 
 urlpatterns = [
-    path(
-        "cdr/",
-        include(cdr_routes),
-    ),
+    path("cdr/", include(cdr_routes)),
 ]
--- cdrplatform/core/views.py
+++ cdrplatform/core/views.py
@@ -1,10 +1,77 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import WeightChoices
+
+
+class ItemInputSerializer(serializers.Serializer):
+    forestation = serializers.IntegerField(
+        required=False,
+        min_value=1,
+    )
+    biooil = serializers.IntegerField(
+        required=False,
+        min_value=1,
+    )
+    kelp = serializers.IntegerField(
+        required=False,
+        min_value=1,
+    )
+    olivine = serializers.IntegerField(
+        required=False,
+        min_value=1,
+    )
+
+
+class BaseAPIView(APIView):
+    """A base API View class for all Views to inherit from.
+
+    Can contain any shared logic of functions.
+    """
+
+
+class CDRPricing(BaseAPIView):
+    """Calculate a carbon dioxide removal price for a given portfolio of
+    CDR items.
+    """
+
+    class InputSerializer(serializers.Serializer):
+        weight_unit = serializers.ChoiceField(
+            required=True,
+            choices=WeightChoices.choices,
+        )
+        # todo: add validation on currency
+        currency = serializers.CharField(min_length=3, max_length=3)
+        items = ItemInputSerializer()
+        # Whether or not the fees should be
+        include_fees = serializers.BooleanField(
+            required=False,
+            default=False,
+        )
+
+    class OutputSerializer(serializers.Serializer):
+        cost = serializers.IntegerField(min_value=0)
+        currency = serializers.CharField(min_length=3, max_length=3)
+
+    @extend_schema(
+        request=InputSerializer,
+        responses={
+            status.HTTP_201_CREATED: OutputSerializer,
+        },
+        methods=("POST",),
+    )
+    def post(self, request):
+        serializer = self.InputSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(("POST",))
-def cdr_pricing(request):
+def cdr_removal_request(request):
     class ItemInputSerializer(serializers.Serializer):
         forestation = serializers.IntegerField(
             required=False,
@@ -26,18 +93,13 @@ def cdr_pricing(request):
     class InputSerializer(serializers.Serializer):
         weight_unit = serializers.ChoiceField(
             required=True,
-            choices=(("g", "gram"), ("kg", "kilogram"), ("t", "tonne")),
+            choices=WeightChoices.choices,
         )
+        currency = serializers.CharField(min_length=3, max_length=3)
         items = ItemInputSerializer()
 
-    if request.method == "POST":
-        serializer = InputSerializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    class OutputSerializer(serializers.Serializer):
+        request_id = serializers.UUIDField()
 
-
-@api_view(("POST",))
-def cdr_removal_request(request):
     if request.method == "POST":
         return Response({"foo": request.version})
