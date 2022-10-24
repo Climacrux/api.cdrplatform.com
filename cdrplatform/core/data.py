6e7b429092db3c2d65e5e24bccad0f32029028b1
Fabienne Muff <fabienne.muff@sunrise.net>
2022-10-24 17:50:29 +0200

    adding fees

---
+++ cdrplatform/core/data.py
@@ -0,0 +1,16 @@
+FEES = {
+    "climacrux": {
+        "flat": 1000,
+        "variable_pct": 8,
+    },
+    "payment_processing": {
+        "wise": {
+            "flat": 50,
+            "variable_pct": 0.5,
+        },
+        "stripe": {
+            "flat": 30,
+            "variable_pct": 2.9,
+        },
+    },
+}
