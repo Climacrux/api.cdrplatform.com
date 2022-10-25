f58b74069f28e4b08890fdb6b9ed2b47a1d2cb1c
Fabienne Muff <fabienne.muff@sunrise.net>
2022-10-25 09:35:20 +0200

    added first test

--- cdrplatform/core/tests.py
+++ cdrplatform/core/tests.py
@@ -1,3 +1,38 @@
-# from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import CurrencyChoices, RemovalMethod, RemovalPartner
+
+
+class CDRPricingViewTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        removal_method = RemovalMethod.objects.create(
+            name="forestation", slug="forestation", description=""
+        )
+        RemovalPartner.objects.create(
+            removal_method=removal_method,
+            name="eden",
+            slug="eden",
+            description="",
+            website="example.com",
+            cost_per_tonne=552,
+            currency=CurrencyChoices.USD,
+        )
+        return super().setUpTestData()
+
+    def test_retrieve_price(self):
+        """
+        Ensure we can successfully retrieve a price.
+        """
+        url = reverse("v1:cdr_price")
+        data = {
+            "weight_unit": "t",
+            "currency": "chf",
+            "items": [{"method_type": "forestation", "amount": 10}],
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # check if it is the correct price
+        # check 401 status with bad data
