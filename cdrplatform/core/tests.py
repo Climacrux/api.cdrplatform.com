cb4bf6ba79aab07989e3628ed734890cfae5c68c
Fabienne Muff <fabienne.muff@sunrise.net>
2022-10-25 10:26:10 +0200

    adding test for retrieving price with multiple methods

--- cdrplatform/core/tests.py
+++ cdrplatform/core/tests.py
@@ -8,16 +8,28 @@ from .models import CurrencyChoices, RemovalMethod, RemovalPartner
 class CDRPricingViewTestCase(APITestCase):
     @classmethod
     def setUpTestData(cls) -> None:
-        removal_method = RemovalMethod.objects.create(
+        removal_method_forestation = RemovalMethod.objects.create(
             name="forestation", slug="forestation", description=""
         )
+        removal_method_biooil = RemovalMethod.objects.create(
+            name="bio-oil", slug="bio-oil", description=""
+        )
         RemovalPartner.objects.create(
-            removal_method=removal_method,
+            removal_method=removal_method_forestation,
             name="eden",
             slug="eden",
             description="",
             website="example.com",
             cost_per_tonne=552,
+            currency=CurrencyChoices.USD,
+        )
+        RemovalPartner.objects.create(
+            removal_method=removal_method_biooil,
+            name="charm",
+            slug="charm",
+            description="",
+            website="example.com",
+            cost_per_tonne=60000,
             currency=CurrencyChoices.USD,
         )
         return super().setUpTestData()
@@ -73,5 +85,37 @@ class CDRPricingViewTestCase(APITestCase):
                         "attr": "items.0.method_type",
                     }
                 ],
+            },
+        )
+
+    def test_retrieve_price_multiple(self):
+        """
+        Ensure we can retrieve correct price successfully.
+        """
+        url = reverse("v1:cdr_price")
+        data = {
+            "weight_unit": "t",
+            "currency": "chf",
+            "items": [
+                {"method_type": "forestation", "amount": 10},
+                {"method_type": "bio-oil", "amount": 10},
+            ],
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            response.data,
+            {
+                "cost": {
+                    "items": [
+                        {"method_type": "forestation", "amount": 10, "cost": 5520},
+                        {"method_type": "bio-oil", "amount": 10, "cost": 600000},
+                    ],
+                    "removal": 605520,
+                    "variable_fees": 52654,
+                    "total": 658174,
+                },
+                "currency": "chf",
+                "weight_unit": "t",
             },
         )
