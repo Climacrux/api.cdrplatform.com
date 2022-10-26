25bc371f9061b534b1cc0e57d19175dcf1d6f9d4
Ewan Jones <egcjones@gmail.com>
2022-10-26 10:35:07 +0200

    adding currency_conversion_rates fixture to tests

--- cdrplatform/core/tests.py
+++ cdrplatform/core/tests.py
@@ -41,7 +41,7 @@ class APIKeyMixin:
 
 class CDRPricingViewTestCase(APIKeyMixin, APITestCase):
 
-    fixtures = ("removal_methods_partners",)
+    fixtures = ("removal_methods_partners", "currency_conversion_rates")
 
     @classmethod
     def setUpTestData(cls) -> None:
@@ -146,7 +146,7 @@ class CDRPricingViewTestCase(APIKeyMixin, APITestCase):
 
 class CDRRemovalViewTestCase(APIKeyMixin, APITestCase):
 
-    fixtures = ("removal_methods_partners",)
+    fixtures = ("removal_methods_partners", "currency_conversion_rates")
 
     @classmethod
     def setUpTestData(cls) -> None:
