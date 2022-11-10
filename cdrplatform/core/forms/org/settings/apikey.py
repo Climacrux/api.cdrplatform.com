9e294566581363923f5b823dbbb16714897c6427
Ewan Jones <egcjones@gmail.com>
2022-11-10 16:13:05 +0100

    beautifying api key form

--- cdrplatform/core/forms/org/settings/apikey.py
+++ cdrplatform/core/forms/org/settings/apikey.py
@@ -10,7 +10,21 @@ class NewAPIKeyForm(forms.Form):
         label=_("API key name"),
         max_length=50,
     )
+    name.widget.attrs.update(
+        {
+            "class": """block w-full flex-1 rounded-md border-gray-300
+        focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm""",
+            "placeholder": _("API key name"),
+        }
+    )
+
     test_key = forms.BooleanField(
         required=False,
         label=_("Test key"),
+    )
+    test_key.widget.attrs.update(
+        {
+            "class": """h-4 w-4 rounded border-gray-300
+            text-indigo-600 focus:ring-indigo-500"""
+        }
     )
